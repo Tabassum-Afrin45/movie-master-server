897cefb026b4556f624badc67ba9060498d7a82d
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2026-02-06 16:43:06 +0600

    Connected database with server through CRUD operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express()
 const port = 3000
 
@@ -26,14 +26,50 @@ async function run() {
     const db=client.db('movies_db')
     const movieCollection=db.collection('movies')
     
+    app.get('/movies',async(req,res)=>{
+        const cursor=movieCollection.find()
+        const result=await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/movies/:id',async(req,res)=>{
+        const id=req.params.id
+        const query={_id: new ObjectId(id)}
+        const result=await movieCollection.findOne(query)
+        res.send(result)
+    })
+
     app.post('/movies',async(req,res)=>{
         const newMovie=req.body;
         const result=await movieCollection.insertOne(newMovie)
         res.send(result);
     })
+    
+       app.patch('/movies/:id',async(req,res)=>{
+        const id =req.params.id
+        const updatedMovie=req.body
+        const query={_id:new ObjectId(id)}
+        const update={
+            $set:{
+                title: updatedMovie.title,
+                genre:updatedMovie.genre,
+                releaseYear:updatedMovie.releaseYear,
+                cast:updatedMovie.cast,
+                duration:updatedMovie.duration,
+                postSummary:updatedMovie.postSummary,
+                postUrl:updatedMovie.postUrl,
+                country:updatedMovie.country
+            }
+        }
+        const result =await movieCollection.updateOne(query,update)
+        res.send(result)
+    })
 
        app.delete('/movies/:id',async(req,res)=>{
         const id =req.params.id
+        const query={_id:new ObjectId(id)}
+        const result=await movieCollection.deleteOne(query)
+        res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
